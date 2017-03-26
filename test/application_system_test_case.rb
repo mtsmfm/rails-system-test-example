fe1a4341f64021f5c7f0e97e43be97a8517aa082
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2017-03-26 18:08:48 +0900

    Cosme

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,12 @@
 require "test_helper"
+require "socket"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :docker_chrome
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {url: "http://chrome:4444/wd/hub"}
+
+  def setup
+    host! "http://#{IPSocket.getaddress(Socket.gethostname)}"
+
+    super
+  end
 end
--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -1,13 +1,6 @@
 require "application_system_test_case"
-require "socket"
 
 class PostsTest < ApplicationSystemTestCase
-  driven_by :docker_chrome
-
-  def setup
-    host! "http://#{IPSocket.getaddress(Socket.gethostname)}"
-  end
-
   10.times do |i|
     test "visiting the index #{i}" do
       visit posts_url
